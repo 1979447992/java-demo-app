e091513a05ad5c21aa9c3323e6e40c81bf5c206d
1979447992 <1979447992@qq.com>
2025-08-02 14:47:35 +0800

    Update DemoApplication.java

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.simplewebapp;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,16 +6,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController // 标记这是一个RESTful控制器
+@RestController
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @GetMapping("/hello") // 定义一个GET请求接口，路径为 /hello
+    @GetMapping("/")
     public String hello() {
-        // 返回一段话，方便验证部署是否成功
-        return "Hello from Jenkins Pipeline! This is build #" + System.getenv("BUILD_NUMBER") + " from Spring Boot app!";
+        return "Hello DevOps Demo! Version: 1.0.0";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
     }
 }
