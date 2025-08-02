e66701f4fc1715cf23f8aa83a6e13e13ac2acb35
1979447992 <1979447992@qq.com>
2025-08-02 23:19:07 +0800

    Update DemoApplication.java

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -15,7 +15,7 @@ public class DemoApplication {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello DevOps Demo230555! Version: 1.0.1";
+        return "Hello DevOps Demo 2319! Version: 1.0.1";
     }
 
     @GetMapping("/health")
